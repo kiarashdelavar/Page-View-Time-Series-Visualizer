4f61816cb30bd55bf1a2dd97877eace6eafc90bf
Kiarash Delavar <139377614+kiarashdelavar@users.noreply.github.com>
2024-01-14 22:26:33 +0330

    Update Page View Time Series Visualizer.py

--- Page View Time Series Visualizer.py
+++ Page View Time Series Visualizer.py
@@ -2,7 +2,6 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
 
-# Load the data
 df = pd.read_csv('fcc-forum-pageviews.csv', index_col='date', parse_dates=True)
 df = df[(df['value'] >= df['value'].quantile(0.025)) & (df['value'] <= df['value'].quantile(0.975))]
 
